1944c2e17bb202babf4702a98c7c0da505f3e667
Theodore Konukhov <komolov.f@gmail.com>
2015-01-07 03:33:20 +0300

    still inefficient but works

--- src/conways_game_of_life/core.clj
+++ src/conways_game_of_life/core.clj
@@ -1,68 +1,52 @@
 (ns conways-game-of-life.core
-  [:require [quil.core :as q]])
+  [:require [quil.core :as q]
+            [conways-game-of-life.life :as life]])
 
-(def grid-width 10)
-(def width      500)
-(def height     500)
-(def cell-color {true 0x000000 false 200})
+(def grid-width   10)
+(def board-width  80)
+(def board-height 50)
+(def cell-color   {true  [10 184 55]
+                   false [0 0 0]})
+
+
+(defn trans
+  [coord]
+  (* grid-width coord))
 
 (defn setup []
-  (q/smooth)
-  (q/frame-rate 1)
-  (q/background 0xffffff))
+  (q/frame-rate 25)
+  (q/background 0x0))
 
-(defn init-random-state []
-  (let [cells-count 1500]
-    (loop [cells #{}]
-      (let [new-cells (conj cells {:x      (rand-int (/ width grid-width))
-                                   :y      (rand-int (/ height grid-width))
-                                   :alive? true})]
-        (if (= cells-count (count new-cells))
-          new-cells
-          (recur new-cells))))))
-
-(def game-state (atom (init-random-state)))
-
-(defn count-next-generation
-  [state]
-  (map (partial apply-rules state) state))
-
-
-(defn find-neighbours-for
-  [state point]
-  (let [{:keys [x y]} point
-        nx              [(inc x) (dec x)]
-        ny              [(inc y) (dec y)]]
-    (filter (fn [p]
-              (and (some #(= % (:x p)) nx)
-                   (some #(= % (:y p)) ny)
-                   (:alive? p)))
-            state)))
-
-(defn apply-rules
-  [state cell]
-  (let [n-count (count (find-neighbours-for state cell))
-        alive   (if (:alive? cell)
-                  (not (or (< n-count 2) (> n-count 3)))
-                  (= n-count 3))]
-    (assoc cell :alive? alive)))
-
-(defn next-generation! []
-  (swap! game-state count-next-generation))
-
-(defn draw []
-  (q/background 0xffffff)
+(defn draw-state
+  [life-state]
+  (q/fill 0x0)
+  (q/background 0x0)
   (q/no-stroke)
-  (doseq [point @game-state]
-    (q/fill (cell-color (:alive? point)))
-    (q/rect (* grid-width (:x point))
-            (* grid-width (:y point))
-            grid-width
-            grid-width)))
 
-(q/defsketch game-of-life
-  :title "Conway's Game Of Life"
-  :setup    setup
-  :draw     #((draw) (next-generation!))
-  :features [:keep-on-top]
-  :size     [width height])
+  (doseq [[x y alive] (:board @life-state)]
+    (apply q/fill (cell-color alive))
+    (q/rect (trans x) (trans y) grid-width grid-width)))
+
+(defn next-generation!
+  [life-state]
+  (swap! life-state update-in [:board] life/step)
+  (swap! life-state update-in [:generation] inc))
+
+
+(defn run!
+  ([]
+   (run! (life/random-cells board-width board-height)))
+  ([cells]
+   (let [life-state (atom {:generation 1
+                           :board      (life/init-board board-width
+                                                        board-height
+                                                        cells)})]
+     (q/defsketch game-of-life
+       :title "Conway's Game Of Life"
+       :setup    setup
+       :draw     (fn []
+                   (draw-state life-state)
+                   (next-generation! life-state))
+       :features [:keep-on-top]
+       :size     (map (partial * grid-width) [board-width
+                                              board-height])))))
---
+++ src/conways_game_of_life/life.clj
@@ -0,0 +1,92 @@
+(ns conways-game-of-life.life
+  (:require [clojure.set :as set]
+            [clojure.string :as s]))
+
+(defn random-cells
+  "Generates random 30% population"
+  [w h]
+  (let [population (int (* 0.3 w h))]
+    (set
+      (take population
+            (shuffle
+             (for [x (range w)
+                   y (range h)] [x y]))))))
+
+(def glider-gun
+  " ........................O
+    ......................O.O
+    ............OO......OO............OO
+    ...........O...O....OO............OO
+    OO........O.....O...OO
+    OO........O...O.OO....O.O
+    ..........O.....O.......O
+    ...........O...O
+    ............OO
+
+    Gosper's glider gun initial cells"
+  #{[24 0] [22 1] [24 1]
+    [12 2] [13 2] [20 2] [21 2] [34 2] [35 2]
+    [11 3] [15 3] [20 3] [21 3] [34 3] [35 3]
+    [0 4] [1 4] [10 4] [16 4] [20 4] [21 4]
+    [0 5] [1 5] [10 5] [14 5] [16 5] [17 5] [22 5] [24 5]
+    [10 6] [16 6] [24 6]
+    [11 7] [15 7]
+    [12 8] [13 8]})
+
+(defn init-board
+  "Inits the board"
+  ([w h]
+   (init-board w h (random-cells w h)))
+  ([w h c-coords]
+   (apply sorted-set
+          (for [x (range w)
+                y (range h)
+                :let [has-cell (seq (set/select #(= [x y] %)
+                                                c-coords))]]
+            (if has-cell [x y true] [x y false])))))
+
+(defn cell-alive?
+  [cell]
+  (last cell))
+
+(defn neighbours
+  [cell]
+  (let [[x y] cell]
+    (set
+      (for [nx    [-1 0 1]
+            ny    [-1 0 1]
+            :let  [n [(+ nx x) (+ ny y) true]]
+            :when (not (= n cell))]
+        n))))
+
+(defn count-neighbours
+  [state cell]
+  (count (set/intersection state
+                           (neighbours cell))))
+
+(defn apply-rules
+  [state cell]
+  (let [cn    (count-neighbours state cell)
+        alive (if (cell-alive? cell)
+                  (or (= 2 cn) (= 3 cn))
+                  (= 3 cn))]
+    (conj (pop cell) alive)))
+
+(defn step
+  [state]
+  (apply sorted-set
+         (map (partial apply-rules state) state)))
+
+(defn run-life
+  [board]
+  (iterate step board))
+
+(defn print-step
+  [board]
+  (println)
+  (print (s/join "\n"
+                 (for [cells (vals (group-by second board))]
+                   (s/join
+                    " "
+                    (map #(if (last %) "o" ".") cells)))))
+  (println))
